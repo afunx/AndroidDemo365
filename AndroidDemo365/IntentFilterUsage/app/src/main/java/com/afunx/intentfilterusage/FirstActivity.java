8bb5969f2b9de48dfc2406da52181c8ba293cc1e
afunx <afun1989@163.com>
2016-09-08 21:01:09 +0800

    IntentFilterUsage is added

---
+++ AndroidDemo365/IntentFilterUsage/app/src/main/java/com/afunx/intentfilterusage/FirstActivity.java
@@ -0,0 +1,14 @@
+package com.afunx.intentfilterusage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+}
--- AndroidDemo365/IntentFilterUsage/app/src/main/java/com/afunx/intentfilterusage/MainActivity.java
+++ AndroidDemo365/IntentFilterUsage/app/src/main/java/com/afunx/intentfilterusage/MainActivity.java
@@ -1,38 +1,45 @@
 package com.afunx.intentfilterusage;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        startFirstActivity();
+//        startSecondActivity();
+//        startThirdActivity();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    // explicit start activity
+    private void startFirstActivity(){
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    // implicit start activity by action
+    private void startSecondActivity(){
+        Intent intent = new Intent();
+//        intent.setAction("com.afunx.action.explicit");
+        intent.setAction("action");
+//        intent.addCategory("category");
+        // don't forget add <category android:name="android.intent.category.DEFAULT"/> in <intent-filter>
+        // or Android System can't find the intent
+        startActivity(intent);
+        // Conclusion: intent action should be one of intent-filter, intent category should no more than of intent-filter
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+    // implicit start activity by data
+    private void startThirdActivity(){
+        Intent intent = new Intent();
+        intent.setType("video/mpeg");
+        startActivity(intent);
     }
 }
